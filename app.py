4bb31fae0242e29f27d643ef1b108a0417d4c429
divyamishra2385-glitch <divyamishra2385@gmail.com>
2026-06-24 18:54:24 +0530

    learnt veriable and data type

---
+++ app.py
@@ -0,0 +1,38 @@
+from encodings.punycode import T
+
+
+var = "Divya"
+print(var)
+var = "Charlie"
+print(var)
+#data types
+# String (str)
+# used fortext value
+name = "divya"
+#integar (int)
+# used for whole no.
+age = 22
+#float
+# used for decimal no.
+Height =5.2
+#boolean (bool)
+#used for True & falue value
+is_student = True 
+#lists
+# ordered mutable collection (which can be changed) of items
+fruits = ["apple","banana"]
+#dictionary (dict)
+#store data as key value pairs
+person = {
+"name": "Divya",
+"age": 25
+}
+#set 
+# unordered collection of unique items
+numbers = { 1,2,3,}
+#NoneType (none)
+# represents absence of a value
+data = None 
+#tuple
+# ordered immutable collection of items (cant be changed)
+coordinates = (10, 20)
